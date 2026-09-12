5287a808719dd1863442a967876007472683792b
Dennis Colak <dennis.colak@pinecrest.edu>
2026-09-12 10:49:19 -0400

    learning from 12808

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.FlywheelShooter;
+
+@TeleOp(name = "Basic TeleOp")
+public class BasicTeleOp extends OpMode {
+	private Robot robot;
+	private InitMenu initMenu;
+
+	@Override
+	public void init() {
+		robot = new Robot(this, true);
+		initMenu = new InitMenu(this);
+	}
+
+	@Override
+	public void init_loop() {
+		initMenu.update();
+	}
+
+	@Override
+	public void start() {
+		robot.setAlliance(initMenu.getAlliance());
+	}
+
+	@Override
+	public void loop() {
+		robot.follower.manual(
+				-gamepad1.left_stick_y,
+				gamepad1.left_stick_x,
+				gamepad1.right_stick_x
+		);
+
+		if (gamepad1.aWasPressed()) {
+			robot.intake.toggleIntake().schedule();
+		}
+		if (gamepad1.bWasPressed()) {
+			robot.intake.outtake().schedule();
+		}
+		if (gamepad1.rightBumperWasPressed()) {
+			robot.flywheel.toggleSpin().schedule();
+		}
+		if (gamepad1.dpadUpWasPressed()) {
+			robot.flywheel.setSpinPosition(FlywheelShooter.SpinPosition.FAR);
+		}
+		if (gamepad1.dpadDownWasPressed()) {
+			robot.flywheel.setSpinPosition(FlywheelShooter.SpinPosition.CLOSE);
+		}
+
+		robot.periodic();
+	}
+
+	@Override
+	public void stop() {
+		robot.stop();
+	}
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitMenu.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class InitMenu {
+	private final OpMode opMode;
+	private final TelemetryManager panels;
+
+	private Alliance alliance = Alliance.BLUE; // alliance selection
+	private double delaySeconds = 0; // delay before running program (for team play)
+
+	public InitMenu(OpMode opMode) {
+		this.opMode = opMode;
+		this.panels = PanelsTelemetry.INSTANCE.getTelemetry();
+		opMode.gamepad1.rumble(500); // reminder to set up the match before starting
+	}
+
+	public void update() {
+		Gamepad g = opMode.gamepad1;
+
+		if (g.leftBumperWasPressed()) {
+			alliance = Alliance.BLUE;
+		} else if (g.rightBumperWasPressed()) {
+			alliance = Alliance.RED;
+		}
+
+		if (g.dpadUpWasPressed()) {
+			delaySeconds += 0.5;
+		} else if (g.dpadDownWasPressed()) {
+			delaySeconds = Math.max(0, delaySeconds - 0.5);
+		}
+
+		panels.debug("===== INIT MENU =====", "Alliance (bumpers L/R): " + alliance, "Delay (dpad up/down): " + delaySeconds + "s");
+		panels.update(opMode.telemetry);
+	}
+
+	public Alliance getAlliance() {
+		return alliance;
+	}
+
+	public double getDelaySeconds() {
+		return delaySeconds;
+	}
+}

4ee8728a8e91fc1551d274433afea9c573359d6b
Dennis Colak <dennis.colak@pinecrest.edu>
2026-09-12 18:36:00 -0400

    custom flywheel controller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/shooting/FlywheelController.java
@@ -0,0 +1,120 @@
+package org.firstinspires.ftc.teamcode.subsystems.shooting;
+
+import com.pedropathing.controllers.Controller;
+import com.pedropathing.controllers.filters.KalmanFilter;
+
+public class FlywheelController {
+	public enum State {
+		SPINUP,
+		READY,
+		COAST,
+	}
+
+
+	public static final double MAX_POWER = 0.98;
+
+	// thresholds for state transitions
+	public static final double READY_ENTER = 0.95;
+	public static final double READY_EXIT = 0.9;
+	public static final double COAST_ENTER = 1.1;
+	public static final double COAST_EXIT = 1.05;
+
+	// kalman filter
+	public static final double STATE_STDDEV = 2;
+	public static final double MEASUREMENT_STDDEV = 0.2;
+
+	// TODO - tune
+	public static double kP = 0.0;
+	public static double kV = 0.0;
+	public static double kS = 0.0;
+
+	private final KalmanFilter kalmanFilter;
+	private final Controller readyController;
+	private double target = 0.0;
+	private double error = 0.0;
+	private State state = State.SPINUP;
+
+
+	public FlywheelController() {
+		kalmanFilter = new KalmanFilter(STATE_STDDEV, MEASUREMENT_STDDEV);
+		readyController = Controller.sum( // proportional + feedforward + static
+				Controller.proportional(kP), Controller.proportionalFeedforward(kV), Controller.staticFeedforward(kS));
+	}
+
+
+	public double update(double measuredRPM) {
+		// update kalman filter
+		kalmanFilter.update(measuredRPM);
+		double filteredRPM = kalmanFilter.state();
+
+
+		// update error
+		error = target - filteredRPM;
+
+		updateState(filteredRPM);
+		return calculate();
+	}
+
+
+	private void updateState(double filteredRPM) {
+		switch (state) {
+			case SPINUP:
+				if (filteredRPM >= target * READY_ENTER) {
+					state = State.READY;
+				}
+				break;
+			case READY:
+				if (filteredRPM < target * READY_EXIT) {
+					state = State.SPINUP;
+				} else if (filteredRPM > target * COAST_ENTER) {
+					state = State.COAST;
+				}
+				break;
+			case COAST:
+				if (filteredRPM < target * COAST_EXIT) {
+					state = State.READY;
+				}
+				break;
+		}
+	}
+
+
+	private double calculate() {
+		switch (state) {
+			case SPINUP:
+				return MAX_POWER;
+			case READY:
+				return Math.max(0.0, Math.min(1.0, readyController.calculate(target, error)));
+			case COAST:
+			default:
+				return 0.0; // coast
+		}
+	}
+
+
+	public void setTarget(double targetRPM) {
+		target = Math.max(0.0, targetRPM);
+	}
+
+	public void reset(double measuredRPM) {
+		kalmanFilter.reset(measuredRPM, 1.0, 1.0);
+		error = 0.0;
+		state = State.SPINUP;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public double getError() {
+		return error;
+	}
+
+	public double getFilteredRPM() {
+		return kalmanFilter.state();
+	}
+
+	public double getTarget() {
+		return target;
+	}
+}
